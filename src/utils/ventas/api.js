c9d0a727f52def96451471aa0e3a3d9a53752f2c
Alejandra Ospina <aaoperalta@hotmail.com>
2021-11-12 16:51:15 -0500

    enlace a repositorio heroku

--- src/utils/ventas/api.js
+++ src/utils/ventas/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:8080"
+const baseURL = "https://young-savannah-31565.herokuapp.com"
 
 const getToken = () => {
   return `Bearer ${localStorage.getItem("token")}`;
